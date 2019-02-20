98b9de39ba41942e427ad10ecb4ce3d29472d6de
Dmitry Popov <dmitry@parsec-club.ru>
2019-02-20 19:56:21 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TransparencyMergeDemo
+{
+    using System;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var bg = Bitmap.FromFile("bg.png");
+            var cover = Bitmap.FromFile("cover.png");
+
+            using (var g = Graphics.FromImage(bg))
+            {
+                g.DrawImage(cover, 0, 0);
+            }
+
+            bg.Save($"out-{Environment.OSVersion.Platform}.png", ImageFormat.Png);
+        }
+    }
+}
