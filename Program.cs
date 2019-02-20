6dfffec201f492e99406c5fe52cbf0cd145207f2
Dmitry Popov <dmitry@parsec-club.ru>
2019-02-20 20:39:20 +0300

    ImageSharp added

--- Program.cs
+++ Program.cs
@@ -3,20 +3,36 @@
     using System;
     using System.Drawing;
     using System.Drawing.Imaging;
+    using SixLabors.ImageSharp.Processing;
+    using SixLabors.ImageSharp;
+    using ISImage = SixLabors.ImageSharp.Image;
 
     public static class Program
     {
         public static void Main(string[] args)
         {
-            var bg = Bitmap.FromFile("bg.png");
-            var cover = Bitmap.FromFile("cover.png");
-
-            using (var g = Graphics.FromImage(bg))
+            using (var bg = Bitmap.FromFile("bg.png"))
             {
-                g.DrawImage(cover, 0, 0);
+                using (var cover = Bitmap.FromFile("cover.png"))
+                {
+
+                    using (var g = Graphics.FromImage(bg))
+                    {
+                        g.DrawImage(cover, 0, 0);
+                    }
+                }
+                bg.Save($"out-drawingcommon-{Environment.OSVersion.Platform.ToString().ToLowerInvariant()}.png", ImageFormat.Png);
             }
 
-            bg.Save($"out-{Environment.OSVersion.Platform}.png", ImageFormat.Png);
+            using (var bg = ISImage.Load("bg.png"))
+            {
+                using (var cover = ISImage.Load("cover.png"))
+                {
+                    bg.Mutate(o => o.DrawImage(cover, 1));
+                }
+
+                bg.Save($"out-imagesharp-{Environment.OSVersion.Platform.ToString().ToLowerInvariant()}.png");
+            }
         }
     }
 }
